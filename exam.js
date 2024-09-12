0871fc635c2e895f4cc1d25122a88464db06d01e
ruviRedner <r.y.r7966@gmail.com>
2024-09-12 09:45:00 +0300

    finish Mission1

---
+++ exam.js
@@ -0,0 +1,50 @@
+const Mission1 =(numbers) =>{
+     const  evenNumbers = numbers.filter(num => num % 2 === 0);
+     return evenNumbers
+}
+console.log(Mission1([1, 2, 3, 4, 5, 6, 7,8,9]));
+
+
+const Mission2 =() =>{
+
+}
+
+const Mission3 =() =>{
+
+}
+
+const Mission4 =() =>{
+
+}
+
+const Mission5 =() =>{
+
+}
+
+const Mission6 =() =>{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+    Mission1,
+    Mission2,
+    Mission3,
+    Mission4,
+    Mission5,
+    Mission6
+}
+   
